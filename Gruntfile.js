0c42f53337535ae105ffb28ec363e4ce29ee9aae
Stephen Underwood <_@sphvn.com>
2015-05-07 16:01:34 +0800

    update dest from example to main

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
       all: {
         src: [ "src/**/*.purs"
              , "bower_components/**/src/**/*.purs"],
-        dest: "dist/example.js"
+        dest: "dist/main.js"
       }
     },
     watch: {
